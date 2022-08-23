733e91da727734ee4ff4523256566380c98af087
gatwi <faith.murithi@student.moringaschool.com>
2022-08-23 21:27:41 +0300

    metaprogramming

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,13 @@
 class Person
-  # your code here
+  attr_accessor :name, :weight, :wrist_size
+ # calling the attr_accessor method through getter and setter
+ 
+  def initialize(attributes = nil)
+    if attributes
+      attributes.each do |key, value|
+        self.class.attr_accessor(key)
+        self.send("#{key}=", value)
+      end
+    end
+  end
 end
